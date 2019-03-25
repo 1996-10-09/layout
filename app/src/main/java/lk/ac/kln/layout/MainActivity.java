bf21077cf81cc4056400d8bf003d951bc983c106
fct <oshdee2kaushalya@gmail.com>
2019-03-25 10:04:52 +0530

    complete buttons in calculator

--- app/src/main/java/lk/ac/kln/layout/MainActivity.java
+++ app/src/main/java/lk/ac/kln/layout/MainActivity.java
@@ -4,16 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
     }
     public void buttonClick(View view){
+        editText.setText("1");
 
+    }
+    public void buttonClick2(View view){
+        editText.setText("2");
 
     }
 }
